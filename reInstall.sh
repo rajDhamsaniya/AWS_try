d771e1176cb9ec92999b2babec52e620a36a3493
rajDhamsaniya <rajdhamsaniya77@gmail.com>
2018-08-20 03:12:25 +0530

    bug fix

--- reInstall.sh
+++ reInstall.sh
@@ -18,7 +18,7 @@ function installPHP(){
 }
 
 function checkPHPPackages(){
-	for i in $*
+	for i in php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
 	do
 		find=$(dpkg --list | grep "${i}");
 		if [ "$find" == 0 ]
